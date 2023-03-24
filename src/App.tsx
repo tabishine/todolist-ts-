499574c77243f2e92cefe975db8427258d68a82e
Zhambulova Tabigat <zhambulovat@gmail.com>
2023-03-24 21:05:56 +0600

    todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TaskType, Todolist } from './Todolist';
 
 function App() {
+  
+  let tasks1: Array<TaskType> = [
+    {id: 1, title: "CSS", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "React", isDone: false}
+  ]
+  let tasks2: Array<TaskType> = [
+    {id: 1, title: "Terminator", isDone: true},
+    {id: 2, title: "Marvel", isDone: true},
+    {id: 3, title: "Gentlmen of fortune", isDone: false}
+  ]
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title = "What to learn" tasks = {tasks1}/>
+      <Todolist title = "Movies" tasks = {tasks2}/>
+
     </div>
   );
 }
---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+// function sum(a:number, b:number ) {
+//   alert(a+b);
+// }
+// sum(12, 14);
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
+}
+
+type PropsType = {
+  title: string
+  tasks: Array<TaskType>
+}
+
+export function Todolist(props: PropsType) { //свойство title
+  return (
+    <div>
+      <h3> {props.title} </h3>
+      <div>
+        <input />
+        <button> + </button>
+      </div>
+      <ul>
+        <li> <input type={"checkbox"} checked={props.tasks[0].isDone} /><span>{props.tasks[0].title}</span></li>
+        <li> <input type={"checkbox"} checked={props.tasks[1].isDone} /><span>{props.tasks[1].title}</span></li>
+        <li> <input type={"checkbox"} checked={props.tasks[2].isDone} /><span>{props.tasks[2].title}</span></li>
+      </ul>
+      <div>
+        <button> All </button>
+        <button> Active </button>
+        <button> Completed </button>
+      </div>
+    </div>
+  );
+}
